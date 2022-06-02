0f6d6652a84d62e494dd45a18d4c6de1c9d4a52c
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2022-06-02 15:59:21 -0300

    Add project files.

---
+++ Operator_Overload_Assignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Operator_Overload_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.Id = 1;
+            employee1.FirstName = "Steph";
+            employee1.LastName = "Simms";
+
+            Employee employee2 = new Employee();
+            employee2.Id = 1;
+            employee2.FirstName = "Joe";
+            employee2.LastName = "Buck";
+
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("employee1 is equal to employee2");
+            }
+            else
+            {
+                Console.WriteLine("employee1 is not equal to employee2");
+            }
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if (employee1.Id.Equals(employee2.Id))
+        {
+            return true;
+        }
+        return false;
+        }
+    public static bool operator !=(Employee employee1, Employee employee2)
+        {
+        if (!employee1.Id.Equals(employee2.Id))
+        {
+            return true;
+        }
+        return false;
+        }
+    }
+}
+            
